af47de6d6eec04c0832b0d28bd08471eada8057d
Samuyo <samuroga113@gmail.com>
2024-10-18 14:19:44 +0200

    Feat: add graphic interface

--- src/main/java/edu/badpals/valorantapi/Controller/Controller.java
+++ src/main/java/edu/badpals/valorantapi/Controller/Controller.java
@@ -1,9 +1,9 @@
-package edu.badpals.valorantapi;
+package edu.badpals.valorantapi.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
