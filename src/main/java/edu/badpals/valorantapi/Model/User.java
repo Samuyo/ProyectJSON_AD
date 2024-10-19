c3c5042790901fddeb7967350c621d8a5ea33cd0
DavBaquero <david.baquero.19@gmail.com>
2024-10-19 12:09:36 +0200

    🔧 Feat(User.java) : Add function registerUser and User

--- src/main/java/edu/badpals/valorantapi/Model/User.java
+++ src/main/java/edu/badpals/valorantapi/Model/User.java
@@ -1,4 +1,21 @@
 package edu.badpals.valorantapi.Model;
 
+import java.util.HashMap;
+
 public class User {
+    HashMap<String,String> users = new HashMap<>();
+    private void User(){
+        users.put("Admin", "Dosp!");
+        users.put("Usuario","1234a");
+    }
+
+    private void registerUser(String usuario, String contrasenia){
+        if ((usuario.length() >= 5) && (contrasenia.length() >= 5)){
+            users.put(usuario,contrasenia);
+        } else if (usuario.length() >= 5) {
+            System.out.println("La contrseña de usuario es muy corta");
+        } else{
+            System.out.println("El nombre de usuario es muy corto");
+        }
+    }
 }
