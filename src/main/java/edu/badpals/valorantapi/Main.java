8bc3276934f7b5a0c040eb942fb00bd82ec5c4d3
DavBaquero <david.baquero.19@gmail.com>
2024-10-19 13:20:47 +0200

    🩹 Fix() : fix error for print screen

--- src/main/java/edu/badpals/valorantapi/Main.java
+++ src/main/java/edu/badpals/valorantapi/Main.java
@@ -2,22 +2,27 @@ package edu.badpals.valorantapi;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainview.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/badpals/valorantapi/login.fxml"));
+            primaryStage.setTitle("Agentes");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module edu.badpals.valorantapi {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires java.desktop;
 
 
-    opens edu.badpals.valorantapi to javafx.fxml;
+    opens edu.badpals.valorantapi to javafx.fxml, javafx.graphics;
     exports edu.badpals.valorantapi.Controller to javafx.fxml;
     opens edu.badpals.valorantapi.Controller to javafx.fxml;
 }
