d836f4c85fe7f2baf2b2499ba98481c465d8c609
DavBaquero <david.baquero.19@gmail.com>
2024-10-20 12:16:41 +0200

    🔧 Feat(AgentsController) : add references to fxml

--- src/main/java/edu/badpals/valorantapi/Controller/AgentsController.java
+++ src/main/java/edu/badpals/valorantapi/Controller/AgentsController.java
@@ -1,14 +1,42 @@
 package edu.badpals.valorantapi.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AgentsController {
+
+    @FXML
+    public Label nomeAge;
+
+    @FXML
+    public Label lblAbil4Ag;
+
+    @FXML
+    public Label lblAbil3Ag;
+
+    @FXML
+    public Label lblAbil1Ag;
+
+    @FXML
+    public Label lblAbil2Ag;
+
+    @FXML
+    public Label lblDescAg;
+
+    @FXML
+    public Label lblRolAg;
+
+    @FXML
+    public Button btnReturn;
+
     public void handlebtnReturn(ActionEvent event){
         try {
             // Carga la nueva venta
