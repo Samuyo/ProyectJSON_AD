bb0db2f4d1591caafb6a5c96b793cbfbc72570d4
Samuyo <samuroga113@gmail.com>
2024-10-20 12:14:51 +0200

    Añadido metodo paraguargar usuarios en un txt

--- src/main/java/edu/badpals/valorantapi/Model/User.java
+++ src/main/java/edu/badpals/valorantapi/Model/User.java
@@ -1,13 +1,14 @@
 package edu.badpals.valorantapi.Model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     HashMap<String,String> users = new HashMap<>();
-    private void User(){
-        users.put("Admin", "Dosp!");
-        users.put("Usuario","1234a");
-    }
+
 
     private void registerUser(String usuario, String contrasenia){
         if ((usuario.length() >= 5) && (contrasenia.length() >= 5)){
@@ -16,6 +17,19 @@ public class User {
             System.out.println("La contrseña de usuario es muy corta");
         } else{
             System.out.println("El nombre de usuario es muy corto");
+        }
+    }
+
+    private void saveUser(){
+        users.put("Admin", "Dosp!");
+        users.put("Usuario","1234a");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("user.txt"))) {
+            for (Map.Entry<String, String> entry : users.entrySet()) {
+                writer.write(entry.getKey() + "=" + entry.getValue());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
