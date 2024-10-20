098e44daa78635bfd9f3323dc46a5fcb7c93be09
DavBaquero <david.baquero.19@gmail.com>
2024-10-20 20:09:00 +0200

    🔧 Feat() : add function to do private the password

--- src/main/java/edu/badpals/valorantapi/Controller/LoginController.java
+++ src/main/java/edu/badpals/valorantapi/Controller/LoginController.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -24,7 +25,7 @@ public class LoginController {
     public Label lblLogin;
 
     @FXML
-    public TextField txtPassword;
+    public PasswordField txtPassword;
 
     @FXML
     public Label lblPassword;
