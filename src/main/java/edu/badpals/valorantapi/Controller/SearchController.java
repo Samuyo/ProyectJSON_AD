da45cfceaac12862ff6236d43823b308f85d2044
DavBaquero <david.baquero.19@gmail.com>
2024-10-20 12:17:45 +0200

    🔧 Feat(SearchController) : add references to fxml

--- src/main/java/edu/badpals/valorantapi/Controller/SearchController.java
+++ src/main/java/edu/badpals/valorantapi/Controller/SearchController.java
@@ -1,14 +1,35 @@
 package edu.badpals.valorantapi.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SearchController {
+
+    @FXML
+    public Tab tabAgent;
+
+    @FXML
+    public TextField txtBuscaAg;
+
+    @FXML
+    public Label lblBuscaAg;
+
+    @FXML
+    public Button btnBuscar;
+
+    @FXML
+    public Button btnExit;
+
     public void handlebtnBuscar(ActionEvent event){
         try {
             // Carga la nueva venta
