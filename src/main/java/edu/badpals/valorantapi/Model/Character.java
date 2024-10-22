55eb47c5b4f9a211656372e44be8481c0ae93fce
Samuyo <samuroga113@gmail.com>
2024-10-22 20:43:59 +0200

    Metodos no usados de Character borrados

--- src/main/java/edu/badpals/valorantapi/Model/Character.java
+++ src/main/java/edu/badpals/valorantapi/Model/Character.java
@@ -49,29 +49,4 @@ public class Character {
     public String getUrlImg() {
         return urlImg;
     }
-
-    /* Recorre el JSON y llena el hashMap "Characters" con el nombre del agente y su id */
-    private void agregarPersonajes (String jsonString){
-        // Convertir la cadena JSON a un JSONArray
-        JSONArray jsonArray = new JSONArray(jsonString);
-
-        // Recorrer el JSONArray y agregar al HashMap solo el ID y el nombre
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
-            String id = jsonObject.getString("uuid");
-            String nombre = jsonObject.getString("displayName");
-            Characters.put(id, nombre);
-        }
-    }
-
-
-    /* Recibe como parámetro un nombre dado por el usuario y el hashmap de "Characters" y te devuelve su respectivo ID */
-    private String nameToId (String nombre, HashMap < String, String > personajes) throws Exception {
-        for (HashMap.Entry<String, String> entry : personajes.entrySet()) {
-            if (entry.getValue().equalsIgnoreCase(nombre)) {
-                return entry.getKey(); // Devuelve el ID como String
-            }
-        }
-        throw new Exception("Personaje no encontrado: " + nombre);
-    }
 }
