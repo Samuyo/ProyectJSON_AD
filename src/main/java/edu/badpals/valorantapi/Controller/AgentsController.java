ad8ea3051b034af1ded3f4e8dae8179e2944aa2c
Samuyo <samuroga113@gmail.com>
2024-10-22 20:33:39 +0200

    Prueba labels

--- src/main/java/edu/badpals/valorantapi/Controller/AgentsController.java
+++ src/main/java/edu/badpals/valorantapi/Controller/AgentsController.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import edu.badpals.valorantapi.Model.Character;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,45 +20,35 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class AgentsController {
+
     private String finalURL = "https://valorant-api.com/v1/agents";
 
     @FXML
     public Label nomeAge;
-
     @FXML
     public Label lblAbil4Ag;
-
     @FXML
     public Label lblAbil3Ag;
-
     @FXML
     public Label lblAbil1Ag;
-
     @FXML
     public Label lblAbil2Ag;
-
     @FXML
     public Label lblDescAg;
-
     @FXML
     public Label lblRolAg;
-
     @FXML
     public Button btnReturn;
 
-
     ArrayList<Character> agentes = new ArrayList<>();
-
 
     public void handlebtnReturn(ActionEvent event) {
         try {
-            // Carga la nueva venta
+            // Carga la nueva ventana
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/badpals/valorantapi/search.fxml"));
             Parent root = loader.load();
-
-            // Obtiene el stado actual
+            // Obtiene el estado actual
             Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-
             Scene scene = new Scene(root);
             stage.setScene(scene);
             // Muestra la ventana
@@ -75,7 +66,6 @@ public class AgentsController {
         }
     }
 
-
     private String accesoAPI() {
         StringBuilder response = new StringBuilder();
         try {
@@ -84,7 +74,6 @@ public class AgentsController {
             con.setRequestMethod("GET");
             int responseCode = con.getResponseCode();
             System.out.println("Response Code: " + responseCode);
-
             if (responseCode == HttpURLConnection.HTTP_OK) { // Comprueba si la respuesta es correcta
                 BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 String inputLine;
@@ -104,33 +93,25 @@ public class AgentsController {
     }
 
     private ArrayList<Character> procesarRespuesta(String jsonResponse) {
-
         JSONObject jsonObject = new JSONObject(jsonResponse);
         JSONArray agentsArray = jsonObject.getJSONArray("data");
-
         for (int i = 0; i < agentsArray.length(); i++) {
             JSONObject agent = agentsArray.getJSONObject(i);
             String uuid = agent.getString("uuid");
             String displayName = agent.getString("displayName");
             String description = agent.getString("description");
-
-
             String fullPortrait = agent.get("fullPortrait").toString();
-
-
             String roleName = "No role";  // Valor por defecto
             if (agent.has("role") && !agent.isNull("role")) {
                 JSONObject role = agent.getJSONObject("role");
                 roleName = role.getString("displayName");
             }
-
             JSONArray abilitiesArray = agent.getJSONArray("abilities");
             ArrayList<String> abilityNames = new ArrayList<>();
             for (int j = 0; j < abilitiesArray.length(); j++) {
                 JSONObject ability = abilitiesArray.getJSONObject(j);
                 abilityNames.add(ability.getString("displayName"));
             }
-
             Character character = new Character(uuid, displayName, description, fullPortrait, roleName, abilityNames);
             agentes.add(character);
         }
@@ -142,52 +123,58 @@ public class AgentsController {
             if (agente.getNombre().equals(nombre)) {
                 System.out.println(agente);
                 return agente; // Devuelve el objeto si se encuentra
-            }
-            else {
+            } else {
                 System.out.println("Peto amigo");
             }
         }
         return null;
     }
+
     @FXML
     private void setNomeAge(String nome) {
-        nomeAge.setText(""+nome);
-
+        nomeAge.setText("" + nome);
     }
+
     @FXML
     private void lblDescAg(String nome) {
-        lblAbil4Ag.setText(buscarAgente(nome).getDescripcion());
+        lblDescAg.setText(nome);
     }
+
     @FXML
-    private void lblRolAg(String Descripcion) {
-        lblAbil3Ag.setText(Descripcion);
+    private void lblRolAg(String rol) {
+        lblRolAg.setText(rol);
     }
+
     @FXML
     private void lblAbil1Ag(String Abil1) {
         lblAbil1Ag.setText(Abil1);
     }
+
     @FXML
     private void lblAbil2Ag(String Abil2) {
         lblAbil2Ag.setText(Abil2);
     }
+
     @FXML
     private void lblAbil3Ag(String Abil3) {
-        lblDescAg.setText(Abil3);
+        lblAbil3Ag.setText(Abil3);
     }
+
     @FXML
     private void lblAbil4Ag(String Abil4) {
-        lblRolAg.setText(Abil4);
+        lblAbil4Ag.setText(Abil4);
     }
 
     public void setAgentes(Character character) {
         setNomeAge(character.getNombre());
-        lblDescAg(character.getDescripcion());
+        lblDescAg((character.getDescripcion()));
         lblRolAg(character.getRol());
         lblAbil1Ag(character.getAbilidades().get(0));
         lblAbil2Ag(character.getAbilidades().get(1));
         lblAbil3Ag(character.getAbilidades().get(2));
         lblAbil4Ag(character.getAbilidades().get(3));
     }
+
     public ArrayList<Character> getAgentes() {
         getAgentesJSON();
         return agentes;
--- src/main/java/edu/badpals/valorantapi/Controller/SearchController.java
+++ src/main/java/edu/badpals/valorantapi/Controller/SearchController.java
@@ -1,10 +1,9 @@
 package edu.badpals.valorantapi.Controller;
 
-import edu.badpals.valorantapi.Model.Character; // Asegúrate de importar la clase Character
+import edu.badpals.valorantapi.Model.Character;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,7 +12,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -21,49 +19,42 @@ public class SearchController {
 
     @FXML
     public Tab tabAgent;
-
     @FXML
     public TextField txtBuscaAg;
-
     @FXML
     public Label lblBuscaAg;
-
     @FXML
     public Button btnBuscar;
-
     @FXML
     public Button btnExit;
 
-
-    AgentsController search = new AgentsController(); // Asegúrate de que esta clase tenga el método getAgentes
+    private AgentsController agentsController;
 
     public void handlebtnBuscar(ActionEvent event) {
         String agenteBuscado = txtBuscaAg.getText().trim();
-        Character buscaAgente = buscarAgente(agenteBuscado); // Llama a buscarAgente
-        System.out.println(search.getAgentes());
-        System.out.println(agenteBuscado);
-        System.out.println(buscaAgente.getNombre());
+        Character buscaAgente = buscarAgente(agenteBuscado);
+
         if (buscaAgente != null) {
-            search.setAgentes(buscaAgente);
-
             try {
-
                 // Carga la nueva ventana
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/badpals/valorantapi/agents.fxml"));
                 Parent root = loader.load();
 
+                // Obtiene el controlador de la nueva ventana
+                AgentsController agentsController = loader.getController();
+                agentsController.setAgentes(buscaAgente);
+
                 // Obtiene el estado actual
                 Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
                 Scene scene = new Scene(root);
                 stage.setScene(scene);
+
                 // Muestra la ventana
                 stage.show();
             } catch (IOException e) {
-                System.out.println("Error handlebtnBuscar");
+                System.out.println("Error handlebtnBuscar: " + e.getMessage());
             }
         } else {
-            // Maneja el caso en que no se encuentra el agente
             lblBuscaAg.setText("Agente no encontrado.");
         }
     }
@@ -76,9 +67,9 @@ public class SearchController {
 
             // Obtiene el estado actual
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
             Scene scene = new Scene(root);
             stage.setScene(scene);
+
             // Muestra la ventana
             stage.show();
         } catch (IOException e) {
@@ -87,12 +78,13 @@ public class SearchController {
     }
 
     private Character buscarAgente(String agenteBuscado) {
-        System.out.println(search.getAgentes());
-        for (Character agente : search.getAgentes()) { // Asegúrate de que getAgentes devuelve un ArrayList<Character>
-            if (agente.getNombre().equals(agenteBuscado)) {
-                return agente; // Retorna el agente encontrado
+        AgentsController search = new AgentsController();
+        search.getAgentes();
+        for (Character agente : search.getAgentes()) {
+            if (agente.getNombre().equalsIgnoreCase(agenteBuscado)) {
+                return agente;
             }
         }
-        return null; // Retorna null si no se encuentra el agente
+        return null;
     }
 }
